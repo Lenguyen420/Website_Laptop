ab93500e1d483651f9bdb8d46965c08ddea6bf3a
Lenguyen420 <151104126+Lenguyen420@users.noreply.github.com>
2024-11-17 15:38:44 +0700

    init webLaptop

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="font/themify-icons/themify-icons.css">
+    <title>LAPTOP79</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            
+        </div>
+        <div class="menu">
+            <ul class="listmenu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Giới thiệu</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+        <div id="main">
+            <div class="sidebar">
+                <p>
+                    <i class="ti-list"></i>
+                    danh mục sản phẩm 
+                </p>
+                <ul class="list_sidebar">
+                    <li><a href="">Laptop dell</a></li>
+                    <li><a href="">Laptop asus</a></li>
+                    <li><a href="">Laptop hp</a></li>
+                    <li><a href="">Laptop msi</a></li>
+                    <li><a href="">Laptop lenovo</a></li>
+                    <li><a href="">Laptop acer</a></li>
+                </ul>
+            </div>
+
+            <div class="maincontent">
+                maincontent
+            </div>
+            
+        </div>
+
+        <div class="clear"></div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+        
+    </div>
+</body>
+</html>
